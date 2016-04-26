ef322f751da3a98951c1a7a69c9941df8d879230
Samvid Mistry <mistrysamvid@gmail.com>
2016-04-27 00:15:32 +0530

    Added edittexts and observable data

--- app/src/main/java/in/samvidinfotech/bindingdemo/MainActivity.java
+++ app/src/main/java/in/samvidinfotech/bindingdemo/MainActivity.java
@@ -3,6 +3,8 @@ package in.samvidinfotech.bindingdemo;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import in.samvidinfotech.bindingdemo.databinding.ActivityMainBinding;
 
@@ -13,7 +15,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setUser(new UserData("Samvid", "Mistry"));
+
+        binding.firstName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.getUser().setFirstName(s.toString());
+            }
+        });
+
+        binding.lastName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.getUser().setLastName(s.toString());
+            }
+        });
     }
 }
--- app/src/main/java/in/samvidinfotech/bindingdemo/UserData.java
+++ app/src/main/java/in/samvidinfotech/bindingdemo/UserData.java
@@ -1,15 +1,30 @@
 package in.samvidinfotech.bindingdemo;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by samvidmistry on 26/4/16.
  */
-public class UserData {
+public class UserData extends BaseObservable {
+    @Bindable
     private String mFirstName;
+    @Bindable
     private String mLastName;
 
     public UserData(String firstName, String lastName) {
         mFirstName = firstName;
         mLastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+        notifyPropertyChanged(in.samvidinfotech.bindingdemo.BR.firstName);
+    }
+
+    public void setLastName(String lastName) {
+        mLastName = lastName;
+        notifyPropertyChanged(in.samvidinfotech.bindingdemo.BR.lastName);
     }
 
     public String getFirstName() {
