307d983397aaec6fb98d4bb8cbd06060cb2a9670
Samvid Mistry <mistrysamvid@gmail.com>
2016-07-03 14:45:28 +0530

    added code for part 2 of article.

--- app/src/main/java/in/samvidinfotech/bindingdemo/UserData.java
+++ app/src/main/java/in/samvidinfotech/bindingdemo/UserData.java
@@ -1,8 +1,11 @@
 package in.samvidinfotech.bindingdemo;
 
+import android.content.Context;
 import android.databinding.Bindable;
 import android.databinding.Observable;
 import android.databinding.PropertyChangeRegistry;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 /**
  * Created by samvidmistry on 26/4/16.
@@ -11,6 +14,7 @@ public class UserData implements Observable {
     private PropertyChangeRegistry mPropertyChangeRegistry;
     private String mFirstName;
     private String mLastName;
+    private boolean mIsAdult = false;
 
     public UserData(String firstName, String lastName) {
         mFirstName = firstName;
@@ -26,6 +30,16 @@ public class UserData implements Observable {
     public void setLastName(String lastName) {
         mLastName = lastName;
         mPropertyChangeRegistry.notifyChange(this, in.samvidinfotech.bindingdemo.BR.lastName);
+    }
+
+    @Bindable
+    public boolean isAdult() {
+        return mIsAdult;
+    }
+
+    public void setAdult(boolean adult) {
+        mIsAdult = adult;
+        mPropertyChangeRegistry.notifyChange(this, in.samvidinfotech.bindingdemo.BR.adult);
     }
 
     @Bindable
@@ -46,5 +60,9 @@ public class UserData implements Observable {
     @Override
     public void removeOnPropertyChangedCallback(OnPropertyChangedCallback onPropertyChangedCallback) {
         mPropertyChangeRegistry.remove(onPropertyChangedCallback);
+    }
+
+    public void toastCheckboxState(Context context, CheckBox checkBox) {
+        Toast.makeText(context, checkBox.isChecked() + "", Toast.LENGTH_SHORT).show();
     }
 }
